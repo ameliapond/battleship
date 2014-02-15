560ba87fe7bd9507a5127074f2b8654e78939925
raxacoricofallapatorius <catin.ronald@gmail.com>
2014-02-15 17:30:44 +0100

    Update function.h

--- function.h
+++ function.h
@@ -1,32 +1,48 @@
-#define _dim1	10
-#define _dim2	10
+#include <string.h>
+
+#define _DIM1		10
+#define _DIM2		10
 
 /*
  * @Brief	Displays a two dimensions's integers table.
  */ 
-void display_int_table(int tableau[_dim1][_dim2]){
+void display_int_int_table(int tableau[_DIM1][_DIM2]){
 
 	int i, j;
 	
-	for (i = 0; i < _dim1; i++)
+	for (i = 0; i < _DIM1; i++)
 	{
-		for (j = 0; j < _dim2; j++)
+		for (j = 0; j < _DIM2; j++)
 		{
 			printf("%d\n", tableau[i][j]);
 		}
 	}
 }
 
+/* 
+ * @Brief	Display the table of charater given in parameter. 
+ */
+void display_char_table(char *table){
+
+	int i;
+	size_t size = strlen(table);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%c",table[i]);
+	}
+}
+
 /*
  * @Brief Set some value in the table *
  */
-void setPlayerTable(int table[_dim1][_dim2]){
+void setPlayerTable(int table[_DIM1][_DIM2]){
 	
 	int i , j ;
 
-	for (i = 0; i < _dim1; i++)
+	for (i = 0; i < _DIM1; i++)
 	{
-		for ( j = 0; j < _dim2; j++)
+		for ( j = 0; j < _DIM2; j++)
 		{
 			if ( i == j )
 			{
