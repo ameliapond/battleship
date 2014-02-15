5fb67e9f2bf52cad405c9549bf6bac5dfa11f0dc
raxacoricofallapatorius <catin.ronald@gmail.com>
2014-02-15 11:40:06 +0100

    Update game.c

--- game.c
+++ game.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "game.h"
+#include "function.h"
 
 void main(int argc, char **argv){
 
@@ -8,9 +9,15 @@ void main(int argc, char **argv){
 	
 	char p1[20] = "129.234.33.0";
 	char p2[20] = "129.234.33.1";
-	
+	int match_table[10][10];
+
 	initStringGrille(GAME->plateau);
+	
+	setPlayerTable(GAME->grille1);
+	
+	initMatchTable(match_table);
+	
+	matchGrids_int_to_string(GAME->plateau, GAME->grille1, match_table);
+	
 	afficheGrille(GAME->plateau);
-	initGame(p1,p2,GAME);
-	afficheJoueur(GAME->player1);
 }
