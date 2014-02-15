4ade41b2f3650c51ff251805eac2e07dc8cc75f7
raxacoricofallapatorius <catin.ronald@gmail.com>
2014-02-15 11:39:19 +0100

    Update game.h

--- game.h
+++ game.h
@@ -11,7 +11,12 @@
  * 			 1 deja tiré ici.
  *			 2 bateau présent.
  */
-#define SIZE_PLATEAU 350
+  
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE_PLATEAU 	350
+#define START_MATCH 	29
 
 typedef struct game{
 	char plateau[SIZE_PLATEAU];
@@ -21,11 +26,11 @@ typedef struct game{
 	int grille2[10][10];
 } game;
 
-/* Initialise un tableau de 300 caractère avec une grille en ascii pour une partie
- * de bataille navale.$
+/* @Brief 	Initialise un tableau de SIZE_PLATEAU caracteres avec une grille en ascii pour une partie
+ * de bataille navale.
  */ 
 void initStringGrille(char grille[SIZE_PLATEAU]){
-	char battlefield[SIZE_PLATEAU] = "    1 2 3 4 5 6 7 8 9 10\n  1 . . . . . . . . . . \n  2 . . . . . . . . . . \n  3 . . . . . . . . . . \n  4 . . . . . . . . . .\n  5 . . . . . . . . . . \n  6 . . . . . . . . . .\n  7 . . . . . . . . . . \n  8 . . . . . . . . . .\n  9 . . . . . . . . . . \n 10 . . . . . . . . . . \n";
+	char battlefield[SIZE_PLATEAU] = "    1 2 3 4 5 6 7 8 9 10\n  1 . . . . . . . . . . \n  2 . . . . . . . . . . \n  3 . . . . . . . . . . \n  4 . . . . . . . . . . \n  5 . . . . . . . . . . \n  6 . . . . . . . . . . \n  7 . . . . . . . . . . \n  8 . . . . . . . . . . \n  9 . . . . . . . . . .\n 10 . . . . . . . . . . \n";
 	int i;
 	for ( i = 0; i < SIZE_PLATEAU; i ++){
 		grille[i] = battlefield[i];
@@ -33,7 +38,7 @@ void initStringGrille(char grille[SIZE_PLATEAU]){
 }
 
 /*
- * @Description Initialise les joueur d'une partie en donnant leur ardresse IP.
+ * @Brief 	Initialise les joueur d'une partie en donnant leur ardresse IP.
  */ 	
 void initGame(char p1[20], char p2[20], game *GAME){
 	int i, j;
@@ -44,13 +49,16 @@ void initGame(char p1[20], char p2[20], game *GAME){
 	}
 }
 
+/*
+ * @Brief 	Gere le deroulement d'une partie.
+ */
 int play(game GAME){
 	return 0;
 }
 
 
 /*
- * Initialise la grille en mettant toutes les cases a 0 (ocean). 
+ * @Brief 	Initialise la grille en mettant toutes les cases a 0 (ocean). 
  */
 void initGrille(int grille[10][10]){
 	int i;
@@ -62,30 +70,31 @@ void initGrille(int grille[10][10]){
 	}
 }	
 
-/* Renvoie 1 si le placement d'un bateau est possible, 
- * -1 si les coordonnees indiquees sont hors de la grille
- * 0 si il y a deja un navire a ces coordonnees.
+/* 
+ * @Brief 	Return 1 if the position is valide to set a boat or 0 else.
+ * @Param	x 		x position to check.
+ * @Param	y 		y position to check.
+ * @Param	grille[][]	
  */
 int navirePlacable(int x, int y, int grille[10][10]){
-	int place; /* indique le type d'erreur pour le placement du bateau */
 	
-	/* coordinate out of range */
-	if ((x >= 0 && x <= 10) && (y >= 0 && y <= 10)){
-		place = -1;
-	}
-	/* Cell already used */
-	if ( grille[x][y] == 2 ){
-		place = 0;
-	}
-	/* ocean */
-	else if(grille[x][y] == 0){
-		place = 1;
+	int place = 0;
+	
+	/* if coordinate are in range and the cell targted is valide i.e
+	 * no previous shoot on it or not boat on it
+	*/
+	if ((x >= 0 && x <= 10) && (y >= 0 && y <= 10))
+	{
+		if ( grille[x][y] == 0 )
+		{
+			place = 1;
+		}
 	}
 	return place;
 }
 
 /* 
- * affiche la grille du jeu 
+ * @Desription	Affiche la grille du jeu 
  */
 void afficheGrille(char grille[SIZE_PLATEAU]){
 	int i;
@@ -95,7 +104,7 @@ void afficheGrille(char grille[SIZE_PLATEAU]){
 }	
 
 /* 
- * Display the table of size 20 given in parameter. 
+ * @Brief	Display the table of size 20 given in parameter. 
  */
 void afficheJoueur(char joueur[20]){
 	int i;
@@ -104,7 +113,61 @@ void afficheJoueur(char joueur[20]){
 	}
 }
 
+/* 
+ * @Descrption	Actualise plateau en fonction de grille
+ * @param 	plateau[] 		Tableau contenant la version chaine de caractere du champ de bataille.
+ * @param	grille[][] 		Tableau d'entier contenant la configuratin de la map du joueur a qui elle appartient.
+ * @param	matching_table[][]	Tableau d'entier contenant la position des elements de la map dans plateau[]
+ */ 
+void matchGrids_int_to_string(char plateau[SIZE_PLATEAU], int grille[10][10], int matching_table[10][10]){
+
+	int i, j;
+
+	for( i = 0; i < 10 ; i++)
+	{
+		for(j = 0; j < 10; j++)
+		{			
+			switch(grille[i][j])
+			{		
+				case 0: plateau[matching_table[i][j]] = '.'; break; 
+				case 1: plateau[matching_table[i][j]] = '0'; break;
+				case 2: plateau[matching_table[i][j]] = 'x'; break;		
+				default:break;
+			}
+		}
+	}
+}
+
+/*
+ * @Description Initialise le tableau de correspondance
+ *		entre les caractere du tableau de caracteres
+ 		et les entier du tableau d'entiers.
+ * @warning	Cette fonction peut etre optimisee.
+ */
+void initMatchTable(int matchTable[10][10]){
+	
+	int 	i, 
+		j,
+		_match = START_MATCH;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			matchTable[i][j] = _match;
+			_match += 2;
+
+		}
+		if ( i < 8 ){
+			_match += 5;
+		}
+		else{ 
+			_match += 4;
+		}
+	}
+}
 /*	
+
    1 2 3 4 5 6 7 8 9 10
  1 . . . . . . . . . . 
  2 . . . . . . . x x . 
@@ -120,4 +183,18 @@ void afficheJoueur(char joueur[20]){
 o : navire placé a flot. 
 x : zone déjà déja tiré.
 . : océan.
+ 
+ table de correspondance entier -> chaine
+ 
+ 29  31  33  35  37  39  41  43  45  47
+ 54  56  58  60  62  64  66  68  70  72
+ 79  81  83  85  87  89  91  93  95  97
+104 106 108 110 112 114 116 118 120 122
+129 131 133 135 137 139 141 143 145 147
+154 156 158 160 162 164 166 168 170 172
+179 181 183 185 187 189 191 193 195 197
+204 206 208 210 212 214 216 218 220 222
+229 231 233 235 237 239 241 243 245 247  
+253 255 257 259 261 263 265 267 269 271
+
 */
